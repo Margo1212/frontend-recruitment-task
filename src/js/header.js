955b587228afe0661382c30110e7f2c40a3d8f7b
Marharyta Leśniak <sirachenko944@gmail.com>
2022-06-13 15:52:27 +0200

    add responsive styles and images

--- src/js/header.js
+++ src/js/header.js
@@ -22,6 +22,25 @@ export const reset = (e) => {
     counter = 0;
 };
 
+const srcset = [
+    "../../images/sean-o-KMn4VEeEPR8-unsplash_1_s6zmfh_ar_1_1,c_fill,g_auto__c_scale,w_200.jpg 200w,",
+    "../../images/sean-o-KMn4VEeEPR8-unsplash_1_s6zmfh_ar_1_1,c_fill,g_auto__c_scale,w_450.jpg 450w,",
+    "../../images/sean-o-KMn4VEeEPR8-unsplash_1_s6zmfh_c_scale,w_480.jpg 480w,",
+    "../../images/sean-o-KMn4VEeEPR8-unsplash_1_s6zmfh_ar_4_3,c_fill,g_auto__c_scale,w_538.jpg 538w,",
+    "../../images/sean-o-KMn4VEeEPR8-unsplash_1_s6zmfh_ar_16_9,c_fill,g_auto__c_scale,w_596.jpg 596w,",
+    "../../images/sean-o-KMn4VEeEPR8-unsplash_1_s6zmfh_ar_1_1,c_fill,g_auto__c_scale,w_630.jpg 630w,",
+    "../../images/sean-o-KMn4VEeEPR8-unsplash_1_s6zmfh_ar_4_3,c_fill,g_auto__c_scale,w_694.jpg 694w,",
+    "../../images/sean-o-KMn4VEeEPR8-unsplash_1_s6zmfh_ar_16_9,c_fill,g_auto__c_scale,w_720.jpg 720w,",
+    "../../images/sean-o-KMn4VEeEPR8-unsplash_1_s6zmfh_c_scale,w_784.jpg 784w,",
+    "../../images/sean-o-KMn4VEeEPR8-unsplash_1_s6zmfh_ar_1_1,c_fill,g_auto__c_scale,w_767.jpg 767w,",
+    "../../images/sean-o-KMn4VEeEPR8-unsplash_1_s6zmfh_c_scale,w_1002.jpg 1002w,",
+    "../../images/sean-o-KMn4VEeEPR8-unsplash_1_s6zmfh_c_scale,w_1234.jpg 1234w,",
+    "../../images/sean-o-KMn4VEeEPR8-unsplash_1_s6zmfh_c_scale,w_1394.jpg 1394w,",
+    "../../images/sean-o-KMn4VEeEPR8-unsplash_1_s6zmfh_c_scale,w_1400.jpg 1400w,",
+  ];
+
+  const sizes = ["(max-width: 1400px) 100vw, 505px"];
+
 export const createHeaderSection = () => {
     const headerSection = createTag({
         tagName: "header",
@@ -38,6 +57,7 @@ export const createHeaderSection = () => {
         className: "header-image",
         tagAttrs: [
             { key: "src", value: "./images/sean-o-KMn4VEeEPR8-unsplash_1_s6zmfh_c_scale,w_1400.jpg" },
+            { key: "srcset", value: srcset},
             { key: "alt", value: "Beautifull beach image" },
           ],
       });
